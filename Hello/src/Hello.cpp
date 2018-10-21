646a3e757066186d12194ec172f5be9d32681898
vhunter <vhunter@vhunter-Lenovo-ideapad-510-15IKB>
2018-10-21 13:54:45 +0100

    Adding the C++ Project From Eclipse to Github

---
+++ Hello/src/Hello.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Mohamed Anis Bouriga
+// Version     :
+// Copyright   : All Rights Reserved!
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
