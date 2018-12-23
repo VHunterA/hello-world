678317a753fd1af7a8da184792e8b591c356b411
VHunterA <m.a.bourigua@gmail.com>
2018-12-23 11:34:34 +0100

    Implementing Client Application : Invoking remote method.

--- HelloClient/src/HelloClient.cpp
+++ HelloClient/src/HelloClient.cpp
@@ -6,10 +6,31 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include "TimeServiceProxy.h"
+#include "TimeServiceClientHelper.h"
+#include "Poco/RemotingNG/TCP/TransportFactory.h"
 #include <iostream>
-using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+
+int main(int argc, char** argv)
+{
+    try
+    {
+        // Register TCP transport.
+        Poco::RemotingNG::TCP::TransportFactory::registerFactory();
+
+        // Get proxy for remote TimeService.
+        std::string uri("remoting.tcp://localhost:9999/tcp/Sample.TimeService/TheTimeService");
+        Sample::ITimeService::Ptr pTimeService = Sample::TimeServiceClientHelper::find(uri);
+
+        // Invoke methods on remote object.
+        std::string currentTime = pTimeService->currentTimeAsString();
+        std::cout << "Current time: " << currentTime << std::endl;
+    }
+    catch (Poco::Exception& exc)
+    {
+        std::cerr << exc.displayText() << std::endl;
+        return 1;
+    }
+    return 0;
 }
